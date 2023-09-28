6840b1ec19d04ab90f695ba6cb3fcaec880dae71
andrew-lucem <andrew@lucemhealth.com>
2023-09-28 03:10:27 -0400

    update go config

--- .config/nvim/lua/pde/go.lua
+++ .config/nvim/lua/pde/go.lua
@@ -6,7 +6,22 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "go", "gomod" })
+			vim.list_extend(opts.ensure_installed, { "go", "gomod", "gowork", "gosum" })
+		end,
+	},
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		opts = function(_, opts)
+			if type(opts.sources) == "table" then
+				local nls = require("null-ls")
+				vim.list_extend(opts.sources, {
+					nls.builtins.code_actions.gomodifytags,
+					nls.builtins.code_actions.impl,
+					nls.builtins.formatting.gofumpt,
+					nls.builtins.formatting.goimports,
+					nls.builtins.formatting.goimports_reviser,
+				})
+			end
 		end,
 	},
 	{
@@ -68,21 +83,9 @@ return {
 			},
 			setup = {
 				gopls = function(_, _)
-					local format_sync_grp = vim.api.nvim_create_augroup("goimport", {})
-					vim.api.nvim_create_autocmd("bufwritepre", {
-						pattern = "*.go",
-						callback = function()
-							require("go.format").goimport()
-						end,
-						group = format_sync_grp,
-					})
-
 					local lsp_utils = require("base.lsp.utils")
 					lsp_utils.on_attach(function(client, bufnr)
 						local map = function(mode, lhs, rhs, desc)
-							if desc then
-								desc = desc
-							end
 							vim.keymap.set(
 								mode,
 								lhs,
@@ -90,15 +93,27 @@ return {
 								{ silent = true, desc = desc, buffer = bufnr, noremap = true }
 							)
 						end
-			           -- stylua: ignore
-			           if client.name == "gopls" then
-			               map("n", "<leader>ly", "<cmd>GoModTidy<cr>", "Go Mod Tidy")
-			               map("n", "<leader>lc", "<cmd>GoCoverage<Cr>", "Go Test Coverage")
-			               map("n", "<leader>lt", "<cmd>GoTest<Cr>", "Go Test")
-			               map("n", "<leader>ltf", "<cmd>GoTestFunc<Cr>", "Go Test Func")
-			               map("n", "<leader>lR", "<cmd>GoRun<Cr>", "Go Run")
-			               map("n", "<leader>dT", "<cmd>lua require('dap-go').debug_test()<cr>", "Go Debug Test")
-			           end
+
+						if client.name == "gopls" then
+							map("n", "<leader>ly", "<cmd>GoModTidy<cr>", "Go Mod Tidy")
+							map("n", "<leader>lc", "<cmd>GoCoverage<Cr>", "Go Test Coverage")
+							map("n", "<leader>lt", "<cmd>GoTest<Cr>", "Go Test")
+							map("n", "<leader>ltf", "<cmd>GoTestFunc<Cr>", "Go Test Func")
+							map("n", "<leader>lR", "<cmd>GoRun -F<Cr>", "Go Run")
+							map("n", "<leader>dT", "<cmd>lua require('dap-go').debug_test()<cr>", "Go Debug Test")
+
+							if not client.server_capabilities.semanticTokensProvider then
+								local semantic = client.config.capabilities.textDocument.semanticTokens
+								client.server_capabilities.semanticTokensProvider = {
+									full = true,
+									legend = {
+										tokenTypes = semantic.tokenTypes,
+										tokenModifiers = semantic.tokenModifiers,
+									},
+									range = true,
+								}
+							end
+						end
 					end)
 				end,
 			},
