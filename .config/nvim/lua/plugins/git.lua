3c129aec397508a7ad7a7028774adefbf7846207
kod4n <kod4n@gmail.com>
2024-09-15 17:40:25 -0400

    update git plugins

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -6,39 +6,17 @@ return {
     end,
   },
   {
-    'tpope/vim-fugitive',
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    keys = {
+      { '<leader>gs', '<cmd>Neogit<cr>', desc = '[G]it [S]tatus' },
+    },
     config = function()
-      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-
-      local fugitive = vim.api.nvim_create_augroup('kod4n-fugitive', {})
-      local autocmd = vim.api.nvim_create_autocmd
-      autocmd('BufWinEnter', {
-        group = fugitive,
-        pattern = '*',
-        callback = function()
-          if vim.bo.ft ~= 'fugitive' then
-            return
-          end
-
-          local bufnr = vim.api.nvim_get_current_buf()
-          local opts = { buffer = bufnr, remap = false }
-          vim.keymap.set('n', '<leader>p', function()
-            vim.cmd.Git 'push'
-          end, opts)
-
-          -- rebase always
-          vim.keymap.set('n', '<leader>P', function()
-            vim.cmd.Git { 'pull', '--rebase' }
-          end, opts)
-
-          -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-          -- needed if i did not set the branch up correctly
-          vim.keymap.set('n', '<leader>t', ':Git push -u origin ', opts)
-        end,
-      })
-
-      vim.keymap.set('n', 'gu', '<cmd>diffget //2<CR>')
-      vim.keymap.set('n', 'gh', '<cmd>diffget //3<CR>')
+      require('neogit').setup {}
     end,
   },
   {
