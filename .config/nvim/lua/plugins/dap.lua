606d65ce6a14d3736ee92abe3e01c0785e529bc9
kod4n <kod4n@gmail.com>
2024-10-13 20:35:13 -0400

    update dap for codelldb

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -57,6 +57,16 @@ return {
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
 
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+        command = 'C:\\Users\\andre\\Downloads\\extension\\adapter\\codelldb.exe',
+        args = { '--port', '${port}' },
+        detached = false,
+      },
+    }
+
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
